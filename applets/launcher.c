39f61a72732ea7d94659c3c8f224b9eed1693e5a
jorn <jorn@b067294f-1dea-0310-9683-c47a78595994>
2006-08-19 11:07:11 +0000

    2006-08-19  Jorn Baayen  <jorn@openedhand.com>
    	* COPYING:
    	* applets/clock.c:
    	* applets/launcher.c:
    	* matchbox-panel/mb-panel.c:
    	* matchbox-panel/mb-panel.h:
    
    	License as GPL.
    
    
    
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-panel-2@1383 b067294f-1dea-0310-9683-c47a78595994

--- applets/launcher.c
+++ applets/launcher.c
@@ -3,20 +3,7 @@
  *
  * Author: Jorn Baayen <jorn@openedhand.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Licensed under the GPL v2 or greater.
  */
 
 #include <config.h>
@@ -63,8 +50,6 @@ launcher_data_free (LauncherData *data)
 
         g_slice_free (LauncherData, data);
 }
-
-#define MAX_ARGS 255
 
 /* The next three functions are based on panel-util.c from gnome-panel */
 static char *
@@ -223,6 +208,8 @@ screen_changed_cb (GtkWidget    *widget,
 }
 
 /* Convert command line to argv array, stripping % conversions on the way */
+#define MAX_ARGS 255
+
 static char **
 exec_to_argv (const char *exec)
 {
