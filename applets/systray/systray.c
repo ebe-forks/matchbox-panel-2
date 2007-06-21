5031024899542a9f81a57df8f9e5ea60421ed99a
rob <rob@b067294f-1dea-0310-9683-c47a78595994>
2007-06-21 10:09:20 +0000

    2007-06-21  Rob Bradford  <rob@openedhand.com>
    	* applets/systray/Makefile.am:
    	* applets/systray/eggmarshalers.list:
    	* applets/systray/eggtraymanager.c:
    	* applets/systray/eggtraymanager.h:
    	* applets/systray/na-marshal.list:
    	* applets/systray/na-tray-manager.c: (na_tray_manager_init),
    	(na_tray_manager_class_init), (na_tray_manager_finalize),
    	(na_tray_manager_set_property), (na_tray_manager_get_property),
    	(na_tray_manager_new), (na_tray_manager_plug_removed),
    	(na_tray_manager_make_socket_transparent),
    	(na_tray_manager_socket_exposed),
    	(na_tray_manager_socket_style_set),
    	(na_tray_manager_handle_dock_request), (pending_message_free),
    	(na_tray_manager_handle_client_message_message_data),
    	(na_tray_manager_handle_begin_message),
    	(na_tray_manager_handle_cancel_message),
    	(na_tray_manager_handle_client_message_opcode),
    	(na_tray_manager_window_filter),
    	(na_tray_manager_selection_clear_event),
    	(na_tray_manager_unmanage),
    	(na_tray_manager_set_orientation_property),
    	(na_tray_manager_manage_screen_x11),
    	(na_tray_manager_manage_screen),
    	(na_tray_manager_check_running_screen_x11),
    	(na_tray_manager_check_running), (na_tray_manager_get_child_title),
    	(na_tray_manager_set_orientation),
    	(na_tray_manager_get_orientation):
    	* applets/systray/na-tray-manager.h:
    	* applets/systray/systray.c: (force_icon_redraw), (style_set_cb),
    	(tray_icon_added_cb), (message_sent_cb), (message_cancelled_cb),
    	(screen_changed_cb), (mb_panel_applet_create):
    	Remove the eggtraymanager code and replace with the na-tray-manager
    	code from gnome-panel/applets/notification_area.
    	Force a redraw of the embedded icons when the style is changed to
    	ensure that they get updated.
    
    
    
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-panel-2@1575 b067294f-1dea-0310-9683-c47a78595994

--- applets/systray/systray.c
+++ applets/systray/systray.c
@@ -15,15 +15,35 @@
   #include <libnotify/notify.h>
 #endif
 
-#include "eggtraymanager.h"
+#include "na-tray-manager.h"
+
+/* We need to force the redraw like this because gtk_widget_queue_draw ()
+ * doesn't help.
+ */
+
+static void
+force_icon_redraw (GtkWidget *box)
+{
+       gtk_widget_hide (box);
+       gtk_widget_show (box);
+}
+
+static void
+style_set_cb (GtkWidget *widget, 
+              GtkStyle  *old_style, 
+              gpointer  user_data)
+{
+        force_icon_redraw (widget);
+}
 
 /* Tray icon added */
 static void
-tray_icon_added_cb (EggTrayManager *manager,
+tray_icon_added_cb (NaTrayManager *manager,
                     GtkWidget      *icon,
                     GtkBox         *box)
 {
         gtk_box_pack_start (box, icon, FALSE, FALSE, 0);
+        force_icon_redraw (GTK_WIDGET (box));
 }
 
 #ifdef USE_LIBNOTIFY
@@ -46,7 +66,7 @@ notification_closed_cb (NotifyNotification *n,
 
 /* Message sent */
 static void
-message_sent_cb (EggTrayManager      *manager,
+message_sent_cb (NaTrayManager      *manager,
                  GtkWidget           *icon,
                  const char          *message,
                  long                 id,
@@ -102,7 +122,7 @@ message_sent_cb (EggTrayManager      *manager,
 
 /* Message cancelled */
 static void
-message_cancelled_cb (EggTrayManager *manager,
+message_cancelled_cb (NaTrayManager *manager,
                       GtkWidget      *icon,
                       long            id,
                       gpointer        user_data)
@@ -135,26 +155,28 @@ message_cancelled_cb (EggTrayManager *manager,
 static void
 screen_changed_cb (GtkWidget      *widget,
                    GdkScreen      *old_screen,
-                   EggTrayManager *manager)
+                   NaTrayManager *manager)
 {
         GdkScreen *screen;
 
         screen = gtk_widget_get_screen (widget);
-        if (egg_tray_manager_check_running (screen)) {
+        if (na_tray_manager_check_running (screen)) {
                 g_warning ("Another system tray manager is running. "
                            "Not managing screen.");
 
                 return;
         }
 
-        egg_tray_manager_manage_screen (manager, screen);
+        na_tray_manager_manage_screen (manager, screen);
+
+        force_icon_redraw (widget);
 }
 
 G_MODULE_EXPORT GtkWidget *
 mb_panel_applet_create (const char    *id,
                         GtkOrientation orientation)
 {
-        EggTrayManager *manager;
+        NaTrayManager *manager;
         GtkWidget *box;
 
 #ifdef USE_LIBNOTIFY
@@ -171,8 +193,8 @@ mb_panel_applet_create (const char    *id,
         gtk_widget_set_name (box, "MatchboxPanelSystemTray");
 
         /* Create tray manager */
-        manager = egg_tray_manager_new ();
-        egg_tray_manager_set_orientation (manager, orientation);
+        manager = na_tray_manager_new ();
+        na_tray_manager_set_orientation (manager, orientation);
 
         g_signal_connect (manager,
                           "tray-icon-added",
@@ -194,6 +216,12 @@ mb_panel_applet_create (const char    *id,
                           "screen-changed",
                           G_CALLBACK (screen_changed_cb),
                           manager);
+
+        g_signal_connect (box,
+                          "style-set",
+                          G_CALLBACK (style_set_cb),
+                          NULL);
+
 
         g_object_weak_ref (G_OBJECT (box),
                            (GWeakNotify) g_object_unref,
